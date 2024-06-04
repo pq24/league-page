f21ceda71930820f9b624eaf2d95508cbb5a429e
pq24 <pmquinn24@gmail.com>
2024-06-04 13:31:10 -0400

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -249,7 +249,7 @@ export const managers = [
       "favoriteTeam": "phi", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
       "mode": "Win Now", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
       "rival": {
-        name: "TOM", // Can be anything (usually your rival's name)
+        name: "Paul", // Can be anything (usually your rival's name)
         link: 3, // manager array number within this array, or null to link back to all managers page
         image: "/managers/pQuinn.jpg", // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
       },
